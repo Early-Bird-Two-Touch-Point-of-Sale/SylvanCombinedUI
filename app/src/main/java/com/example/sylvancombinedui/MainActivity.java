2357437ae666d526a302cedaecae178611fa2d5a
Alex Fittinghoff <47123647+AFitting@users.noreply.github.com>
2020-11-13 18:50:35 -0800

    Added login activity.

--- app/src/main/java/com/example/sylvancombinedui/MainActivity.java
+++ app/src/main/java/com/example/sylvancombinedui/MainActivity.java
@@ -1,91 +1,80 @@
 package com.example.sylvancombinedui;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.navigation.NavigationView;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private EditText eUsername;
+    private EditText ePassword;
+    private Button btnLogin;
+    private TextView eAttemptsInfo;
 
-    private DrawerLayout drawer;
+    private String correctUsername = "Admin";
+    private String correctPassword = "Admin";
+    boolean isValid = false;
+    private int counter = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        eUsername = (EditText) findViewById(R.id.etUsername);
+        ePassword = (EditText) findViewById(R.id.etPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        eAttemptsInfo = (TextView) findViewById(R.id.tvAttemptsInfo);
 
-        drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inputUsername = eUsername.getText().toString();
+                String inputPassword = ePassword.getText().toString();
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState(); //rotates hamburger icon together with drawer
+                if (inputUsername.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Please Enter a Username", Toast.LENGTH_SHORT).show();
+                }
+                else if (inputPassword.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Please Enter a Password", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    isValid = validate(inputUsername, inputPassword);
 
-        if (savedInstanceState == null) { //if savedInstanceState is null, then the app is just
-            //being started, and the default fragment can be loaded. However, if it's not null,
-            //it won't be. Since onCreate is called when the phone is rotated, this if-statement
-            //prevents the default fragment from being loaded every time you rotate the phone
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new HomeFragment()).commit(); //sets default fragment
-            navigationView.setCheckedItem(R.id.nav_home);
-        }
+                    if (!isValid){
+                        //decrement counter for login attempts
+                        counter--;
+                        Toast.makeText(MainActivity.this, "Username and/or Password is Invalid",
+                                Toast.LENGTH_SHORT).show();
+                        eAttemptsInfo.setText("No. of Attempts Remaining: " + counter);
+                        if (counter == 0){
+                            btnLogin.setEnabled(false);
+                        }
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(MainActivity.this, UIActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            }
+        });
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId()){ //switch between fragments depending on selection
-            case R.id.nav_home:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new HomeFragment()).commit();
-                break;
-            case R.id.nav_create:
-                startActivity(new Intent(MainActivity.this, OrderActivity.class));
-                break;
-            case R.id.nav_edit:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new EditFragment()).commit();
-                break;
-            case R.id.nav_history:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new HistoryFragment()).commit();
-                break;
-            case R.id.nav_inventory:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new InventoryFragment()).commit();
-                break;
-            case R.id.nav_user_profile: //Toast placeholders
-                Toast.makeText(this, "Your User Profile", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.nav_logout:
-                Toast.makeText(this, "Logged out", Toast.LENGTH_SHORT).show();
-                break;
+    private boolean validate(String username, String password){
+        if(username.equals(correctUsername) && password.equals(correctPassword)){
+            return true;
         }
-
-        drawer.closeDrawer(GravityCompat.START);
-
-        return true; //returning false = no item selected even if action was triggered, so we want
-    }                //to return true
-
-    @Override
-    public void onBackPressed(){
-        if (drawer.isDrawerOpen(GravityCompat.START)){ //use GravityCompat.END b/c the drawer is
-            drawer.closeDrawer(GravityCompat.START);   //on the left side of the screen
-        } else {
-            super.onBackPressed();
+        else {
+            return false;
         }
     }
 }
---
+++ app/src/main/java/com/example/sylvancombinedui/UIActivity.java
@@ -0,0 +1,94 @@
+package com.example.sylvancombinedui;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class UIActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState(); //rotates hamburger icon together with drawer
+
+        if (savedInstanceState == null) { //if savedInstanceState is null, then the app is just
+            //being started, and the default fragment can be loaded. However, if it's not null,
+            //it won't be. Since onCreate is called when the phone is rotated, this if-statement
+            //prevents the default fragment from being loaded every time you rotate the phone
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit(); //sets default fragment
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){ //switch between fragments depending on selection
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HomeFragment()).commit();
+                break;
+            case R.id.nav_create:
+                startActivity(new Intent(UIActivity.this, OrderActivity.class));
+                break;
+            case R.id.nav_edit:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new EditFragment()).commit();
+                break;
+            case R.id.nav_history:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HistoryFragment()).commit();
+                break;
+            case R.id.nav_inventory:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new InventoryFragment()).commit();
+                break;
+            case R.id.nav_user_profile: //Toast placeholders
+                Toast.makeText(this, "Your User Profile", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(this, "Logged out", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(UIActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); //closes this activity
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true; //returning false = no item selected even if action was triggered, so we want
+    }                //to return true
+
+    @Override
+    public void onBackPressed(){
+        if (drawer.isDrawerOpen(GravityCompat.START)){ //use GravityCompat.END b/c the drawer is
+            drawer.closeDrawer(GravityCompat.START);   //on the left side of the screen
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
