acccd2121fc45982b8bfb32cf8ce4ef383677c93
Jay Revolinsky <jr408@humboldt.edu>
2020-11-23 23:45:44 -0800

    Added DBAdapter.java, TO:DO; test functionality so that host db and receiver db make updates to one another

---
+++ app/src/main/java/com/example/sylvancombinedui/DBAdapter.java
@@ -0,0 +1,8 @@
+package com.example.sylvancombinedui;
+
+public class DBAdapter {
+    //DBAdapter hostDBAdapter = new DbAdapter(getApplicationContext());
+    //performerDBAdapter.open();
+    //refer to this stackoverflow article for an interesting idea:
+    // https://stackoverflow.com/questions/7053809/share-sqlite-database-between-2-android-apps#7935032
+}
